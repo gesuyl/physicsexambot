fa1e580bf742287333c001674e403ae5d15b990d
gesuyl <60263310+gesuyl@users.noreply.github.com>
2024-01-10 21:04:38 +0100

    changed config_reader.py to be pydantic-style

--- app/config_reader.py
+++ app/config_reader.py
@@ -1,53 +1,47 @@
-from re import S
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import SecretStr
 import os
 from platform import system
 
 
-
 class Settings(BaseSettings):
     BOT_TOKEN: SecretStr
     STORED_IMAGES_FOLDER: str
     PROC_IMAGES_FOLDER: str
-    TESSERACT_BIN: str = None
+    TESSERACT_BIN: str
 
 
-    # @classmethod
-    # def set_tesseract_bin(cls, settings):
-    platform_to_tesseract = {
-        "Windows": r'C:\Program Files\Tesseract-OCR\tesseract.exe',
-        "Linux": '/usr/bin/tesseract',
-        "Darwin": '/usr/local/bin/tesseract',
-    }
-    TESSERACT_BIN = platform_to_tesseract.get(system(), 'tesseract')
+    @classmethod
+    def set_tesseract_bin(cls):
+        platform_to_tesseract = {
+            "Windows": r'C:\Program Files\Tesseract-OCR\tesseract.exe',
+            "Linux": '/usr/bin/tesseract',
+            "Darwin": '/usr/local/bin/tesseract',
+        }
+        os_ = system()
+        print('Detected OS:', os_)
+              
+        cls.TESSERACT_BIN = platform_to_tesseract.get(os_, 'tesseract')
 
-    print(f'Tesseract binary path set to: {TESSERACT_BIN}')
+        print(f'Tesseract binary path set to: {cls.TESSERACT_BIN}')
 
 
-    def ensure_folders_exist(settings):
-        for folder_path in [settings.STORED_IMAGES_FOLDER, settings.PROC_IMAGES_FOLDER]:
+    def ensure_folders_exist(self):
+        for folder_path in [self.STORED_IMAGES_FOLDER, self.PROC_IMAGES_FOLDER]:
             if not os.path.exists(folder_path):
                 os.makedirs(folder_path)
                 print(f'Created folder: {folder_path}')
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = 'utf-8'
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
     
         
+print('Reading config...')
+
 settings = Settings()
-Settings.ensure_folders_exist(settings)
 
-try:
-    print(settings.TESSERACT_BIN)
-    
-except KeyError as e:
-    if e.args[0] == 'TESSERACT_BIN':
-        print('Tesseract binary path not set.')
-        Settings.set_tesseract_bin(settings)
+if not settings.TESSERACT_BIN:
+    print('Tesseract binary path not set')
+    Settings.set_tesseract_bin()
+    settings.TESSERACT_BIN = Settings.TESSERACT_BIN
 
-print(settings.model_dump_json())
-exit()
-
-config = Settings()
+settings.ensure_folders_exist()
