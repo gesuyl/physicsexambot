09ba54769653a3666e8d63237a74a1114fb19a43
gesuyl <60263310+gesuyl@users.noreply.github.com>
2024-01-15 17:46:24 +0100

    added drop_table() and delete_user() methods, temporarily disabled logging

--- app/database.py
+++ app/database.py
@@ -1,20 +1,40 @@
-import logging
 from sqlalchemy import create_engine, Column, Integer, String, Float, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-logging.getLogger('sqlalchemy.engine').setLevel(logging.CRITICAL)
+Base = declarative_base()
+
+
+class ImageData(Base):
+    __tablename__ = "image_data"
+    id = Column(Integer, primary_key=True)
+    file_name = Column(String, nullable=False)
+    recog_text = Column(Text)
+
+
+class Users(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True)
+    role = Column(String)
+
+    def to_dict(self):
+        return {"id": self.id, "username": self.username, "role": self.role}
+
+
+class MainConf(Base):
+    __tablename__ = "main_conf"
+    id = Column(Integer, primary_key=True)
+    img_proc_count = Column(Integer)
+    precision = Column(Float)
 
 
 class Database:
-
     def __init__(self, db_url):
-        self.engine = create_engine(db_url, echo=True)
-        Base = declarative_base()
+        self.engine = create_engine(db_url)
         Base.metadata.create_all(bind=self.engine)
         self.Session = sessionmaker(bind=self.engine)
-
 
     def add_user(self, id, username, role):
         with self.Session() as session:
@@ -22,6 +42,13 @@ class Database:
             session.add(user)
             session.commit()
 
+    def delete_user(self, username):
+        with self.Session() as session:
+            user = session.query(Users).filter_by(username=username).first()
+            if not user:
+                return
+            session.delete(user)
+            session.commit()
 
     def save_image_data(self, file_name, recog_text):
         with self.Session() as session:
@@ -29,20 +56,17 @@ class Database:
             session.add(image_data)
             session.commit()
 
-
     def save_image_data_batch(self, image_data_list):
         with self.Session() as session:
             session.bulk_save_objects(image_data_list)
             session.commit()
-
 
     def get_image_data(self, image_id=None):
         with self.Session() as session:
             if image_id:
                 return session.query(ImageData).filter_by(id=image_id).first()
             else:
-                return session.query(ImageData).all() 
-
+                return session.query(ImageData).all()
 
     def get_users(self, username=None):
         with self.Session() as session:
@@ -54,44 +78,25 @@ class Database:
     def update_attr(self, count=None, precision=None):
         with self.Session() as session:
             conf = session.query(MainConf).first()
-            if count: conf.img_proc_count = count
-            if precision: conf.precision = precision
+            if count:
+                conf.img_proc_count = count
+            if precision:
+                conf.precision = precision
             session.commit()
-
 
     def get_image_processing_count(self):
         with self.Session() as session:
             main_conf = session.query(MainConf).first()
             if main_conf:
                 return main_conf.img_proc_count
-            else:
-                return 0
+            return 0
 
     def get_precision(self):
         with self.Session() as session:
             main_conf = session.query(MainConf).first()
             if main_conf:
                 return main_conf.precision
-            else:
-                return 0
-        
+            return 0
 
-class ImageData(Base):
-    __tablename__ = 'image_data'
-    id = Column(Integer, primary_key=True)
-    file_name = Column(String, nullable=False)
-    recog_text = Column(Text)
-
-
-class Users(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True)
-    role = Column(String)
-
-
-class MainConf(Base):
-    __tablename__ = 'main_conf'
-    id = Column(Integer, primary_key=True)
-    img_proc_count = Column(Integer)
-    precision = Column(Float)
+    def drop_table(self, db_name):
+        Base.metadata.drop_all(bind=self.engine, tables=[db_name])
