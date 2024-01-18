ec83becffd87cab997d763b6ad76a888f565165b
gesuyl <60263310+gesuyl@users.noreply.github.com>
2024-01-18 11:39:30 +0100

    many changes in image_reader.py (desc)
    - set all ImageReader attributes on init
    - change update_info to update super_admin and photo_text_dict correctly
    - add add_superadmin method for /init command
    - fix fill_db, add verbosity option
    - fix and tweak preprocess_image
    - rename evaluate_ocr_confidence to evaluate_ocr_parameters
    - fix evaluate_ocr_parameters to only use 1 parameter instead of 3
    - add tesseract config, char whitelist to recognize_text

--- app/image_reader.py
+++ app/image_reader.py
@@ -1,24 +1,30 @@
 import os
 import time
+from datetime import datetime
 from copy import copy
-import itertools
 import pytesseract
 import cv2
 import numpy as np
 from PIL import Image
-from tabulate import tabulate
 #
 from app.config.config import settings
-from utils.utils import remove_control_chars
-from database import ImageData
+from app.utils.utils import remove_control_chars, progress_bar
+from app.database import ImageData
 
 
 
 class ImageReader:
-    def __init__(self, dbase):
-        self.processor: object = settings.TESSERACT_BIN
+    def __init__(self, dbase) -> None:
+        # self.processor: object = settings.TESSERACT_BIN
         self.db = dbase
-        # pytesseract.pytesseract.tesseract_cmd = settings.TESSERACT_CMD
+        self.users: dict = {}
+        self.admins: list = []
+        self.super_admin: str = None
+        self.photo_text_dict: dict = {}
+        self.img_count: int = 0
+        self.precision: float = 0
+
+        # pytesseract.pytesseract.tesseract_cmd = settings.TESSERACT_BIN
         if os.path.exists(settings.PROC_IMAGES_FOLDER):
             self.proc_image_folder: str = settings.PROC_IMAGES_FOLDER
         else:
@@ -26,144 +32,224 @@ class ImageReader:
             self.proc_image_folder: str = settings.PROC_IMAGES_FOLDER
 
 
-    def update_info(self):
-        self.users: dict = {user.username: {
-                                    "id": user.id,
-                                    "role": user.role
-                                    }
-                            for user in self.db.get_users()
-                            }
-        self.admins: list = [user for user in self.users.values() if user["role"] == "Admin"]
-        self.img_count: int = self.db.get_image_processing_count()
-        self.precision: float = self.db.get_precision()
-        self.photo_text_dict: dict = self.db.get_image_data()
+    def update_info(self) -> None:
+        self.users = {
+            user.username: {"id": user.id, "role": user.role}
+            for user in self.db.get_users()
+        }
+        self.admins = [user for user in self.users.values() if user["role"] == "admin"]
 
-        print('ImageReader info updated')
+        if (
+            any(user["role"] == "superadmin" for user in self.users.values())
+            and not self.super_admin
+        ):
+            self.super_admin = [
+                username
+                for username, user_data in self.users.items()
+                if user_data.get("role") == "superadmin"
+            ][0]
+            self.admins.append(self.super_admin)
+
+        images = [image_instance.to_dict() for image_instance in self.db.get_images()]
+        self.photo_text_dict = {image["file_name"]: image["recog_text"] for image in images}
+        self.img_count = self.db.get_image_processing_count()
+        self.precision = self.db.get_precision()
+
+        print("[#] ImageReader info updated")
 
 
-    def fill_db(self):
+    def add_superadmin(self, username) -> None:
+        self.super_admin = username
+        self.admins.append(self.super_admin)
+
+
+    def fill_db(self, verbose=False) -> None:
+        files_to_work = os.listdir(settings.STORED_IMAGES_FOLDER)
+        approx_time = round(len(files_to_work)*3, 2)
+        print(
+            f"+{'='*72}+\n"
+            "[*] Filling database with OCR data...\n"
+            f"[#] Start time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n"
+            f"[#] Verbose: {'Yes' if verbose else 'No'}\n"
+            f"[#] Number of images to process: {len(files_to_work)}\n"
+            f"[#] Approximate time: {approx_time} s (~{round(approx_time / 60.0)} mins)"
+        )
+
         image_data_list = []
-        tabulate_data = []
-        tabulate_headers = ["Filename", "Confidence", "Recognized Text"]
-        first_iter = True
 
-        for file_name in os.listdir(settings.STORED_IMAGES_FOLDER):
-            if file_name.endswith('.jpg'):
+        for idx, file_name in enumerate(files_to_work):
+            if not verbose:
+                print(
+                    f"[*] Processed images: {idx}/{len(files_to_work)} ",
+                    f"{progress_bar(idx, len(files_to_work))}",
+                    end="\r"
+                )
+            if file_name.endswith(".jpg"):
                 image_path = os.path.join(settings.STORED_IMAGES_FOLDER, file_name)
 
-                if first_iter:
-                    first_iter = False
-                    
-                    print("Evaluating OCR parameters")
-                    
-                    conf, param, time = self.evaluate_ocr_confidence(image_path)
-                    
-                    print("Evaluation complete")
-                    print(f"Best confidence: {conf}\nBest parameters: {param}\nTotal time: {time}s\n")
-                
-                result = pytesseract.image_to_string(Image.open(image_path), lang="ukr+eng", output_type='dict')
+                if verbose:
+                    print(
+                        f"+{'-'*72}+\n"
+                        f"[*] Processing {file_name}...")
 
-                recognized_text = result['text']
-                confidence = result['conf']
+                conf, best_scale_factor, tot_time = self.evaluate_ocr_parameters(image_path)
 
-                image_data_list.append(ImageData(file_name=file_name, recog_text=recognized_text))
+                if verbose:
+                    print(
+                        f"[#] \t{'Best confidence':<23}: {conf}\n"
+                        f"[#] \t{'Best scale factor':<23}: {best_scale_factor}\n"
+                        f"[#] \t{'Evaluation time':<23}: {tot_time} s"
+                    )
 
-                max_text_length = 30  
-                cropped_text = recognized_text[:max_text_length] + '...' if len(recognized_text) > max_text_length else recognized_text
+                preprocessed_image = self.preprocess_image(
+                    image_path,
+                    fxfy=best_scale_factor
+                )
 
-                tabulate_data.append([file_name, confidence, cropped_text])
+                result = self.recognize_text(preprocessed_image)
+                recognized_text = " ".join(result["text"])
+
+                image_data_list.append(
+                    ImageData(file_name=file_name, recog_text=recognized_text)
+                )
 
                 self.photo_text_dict[file_name] = recognized_text
                 self.img_count += 1
 
-        print(tabulate(tabulate_data, headers=tabulate_headers, tablefmt="fancy_grid", showindex="always"))
-
         self.db.save_image_data_batch(image_data_list)
         self.db.update_attr(count=self.img_count)
 
+        print(
+            "[#] Filling complete, DB updated.\n"
+            f"+{'='*72}+"
+        )
 
-    def preprocess_image(self, image_path):
-        #enhance
-        enhanced_image = cv2.convertScaleAbs(Image.open(image_path), alpha=1.5, beta=30)
 
-        #binarize
-        gray_image = cv2.cvtColor(enhanced_image, cv2.COLOR_BGR2GRAY)
-        binary_image = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-        
-        #denoise
-        kernel = np.ones((3, 3), np.uint8)
-        denoised_image = cv2.morphologyEx(binary_image, cv2.MORPH_OPEN, kernel)
+    def preprocess_image(self, image_path, fxfy=2) -> np.ndarray:
+        try:
+            img = cv2.imread(image_path)
+
+        except (IOError, SyntaxError):
+            print(f"[!] Bad file '{image_path}', skipping...")
+            return
+
+        #resize
+        img = cv2.resize(
+            src=img,
+            dsize=None,
+            fx=fxfy,
+            fy=fxfy
+        )
+
+        #grayscale
+        gray_image = cv2.cvtColor(
+            src=img,
+            code=cv2.COLOR_BGR2GRAY
+        )
+
+        # denoise
+        kernel = np.ones((3,5), np.uint8)
+        denoised_image = cv2.morphologyEx(
+            src=gray_image,
+            op=cv2.MORPH_OPEN,
+            kernel=kernel
+        )
 
         return denoised_image
 
 
-    def evaluate_ocr_confidence(self, image_path):
+    def evaluate_ocr_parameters(self, image_path) -> tuple:
         start_total_time = time.time()
-        
-        alpha_values = (round(x, 1) for x in itertools.chain(range(10, 21), range(11, 21, 2)))
-        beta_values = range(21)
-        kernel_sizes = itertools.product(range(3, 10, 2), repeat=2)
         best_confidence = 0
-        best_parameters = {}
 
-        for alpha in alpha_values:
-            for beta in beta_values:
-                for kernel_size in kernel_sizes:
-                    preprocessed_image = self.preprocess_image(image_path, alpha, beta, kernel_size)
+        fxfy_values = list(range(1, 6))
 
-                    result = self.recognize_text(preprocessed_image)
-                    confidence = result['conf']
+        for fxfy in fxfy_values:
+            preprocessed_image = self.preprocess_image(
+                image_path=image_path,
+                fxfy=fxfy
+            )
 
-                    if confidence > best_confidence:
-                        best_confidence = confidence
-                        best_parameters = {'alpha': alpha, 'beta': beta, 'kernel_size': kernel_size}
+            result = self.recognize_text(preprocessed_image)
+            confidence = round(sum(result["conf"]) / len(result["conf"]), 4)
 
-        total_time = time.time() - start_total_time
+            if confidence > best_confidence:
+                best_confidence = confidence
+                best_scale_factor = fxfy
 
-        return best_confidence, best_parameters, total_time
-    
+        total_time = round(time.time() - start_total_time, 4)
 
-    def recognize_text(self, image):
-        return pytesseract.image_to_string(image, lang='ukr+eng', output_type='dict')
+        return best_confidence, best_scale_factor, total_time
 
 
-    def return_info(self):
+    def recognize_text(self, image) -> dict:
+        ukrainian_letters_upper = ''.join(chr(i) for i in range(1040, 1072))
+        ukrainian_letters_lower = ''.join(chr(i) for i in range(1072, 1104))
+        english_letters_upper = ''.join(chr(i) for i in range(65, 91))
+        english_letters_lower = ''.join(chr(i) for i in range(97, 123))
+        numbers = ''.join(chr(i) for i in range(48, 58))
+        
+        whitelist = (
+            english_letters_upper + english_letters_lower +
+            ukrainian_letters_upper + ukrainian_letters_lower +
+            numbers + ".,:?()<>+-=_%"
+        )
+        config = f'--oem 3 --psm 12 -c tessedit_char_whitelist={whitelist}'
+
+        return pytesseract.image_to_data(
+            image,
+            lang="ukr+eng",
+            config=config,
+            output_type="dict"
+        )
+
+
+    def return_info(self) -> str:
         admins_info = f"Admins:\n<b>{''.join(self.admins)}</b>\n" if self.admins else ""
 
         users_info = [user for user in self.users.keys() if user not in self.admins]
-        users_to_print = f"Allowed Users:\n<b>{''.join(users_info)}</b>" if users_info else ""
-        
+        users_to_print = (
+            f"Allowed Users:\n<b>{''.join(users_info)}</b>" if users_info else ""
+        )
+
         return f"------------\nImages processed: <b>{self.img_count}</b>\n{admins_info}{users_to_print}\n------------"
 
 
-    async def compare(self, file):
-        recog_text = self.recognize_text(Image.open(f"{os.path.dirname(os.path.realpath(__file__))}\\img_processing\\{file}.jpg"))['text']
-        recog_text_no_spaces = recog_text.replace(' ', '')
+    async def compare(self, file) -> dict:
+        recog_text = self.recognize_text(
+            Image.open(
+                f"{os.path.dirname(os.path.realpath(__file__))}\\img_processing\\{file}.jpg"
+            )
+        )["text"]
+        recog_text_no_spaces = recog_text.replace(" ", "")
         chars_recog_text_no_spaces = [
-            char for char in remove_control_chars(recog_text_no_spaces)]
+            char for char in remove_control_chars(recog_text_no_spaces)
+        ]
         diff_dict = {}
         work_with_dict = self.photo_text_dict
         for file_name in copy(work_with_dict).keys():
             if work_with_dict[file_name] == "":
                 work_with_dict.pop(file_name, None)
         for file_name, txt in work_with_dict.items():
-            txt_no_spaces = txt.replace(' ', '')
+            txt_no_spaces = txt.replace(" ", "")
             chars_txt_no_spaces = [char for char in txt_no_spaces]
             len1 = len(chars_recog_text_no_spaces)
             len2 = len(chars_txt_no_spaces)
             diff = abs(len1 - len2)
             if len1 > len2:
-                chars_recog_text_no_spaces = chars_recog_text_no_spaces[:len1-diff]
+                chars_recog_text_no_spaces = chars_recog_text_no_spaces[: len1 - diff]
             elif len2 > len1:
-                chars_txt_no_spaces = chars_txt_no_spaces[:len2-diff]
+                chars_txt_no_spaces = chars_txt_no_spaces[: len2 - diff]
             num = 0
             for i, char in enumerate(chars_recog_text_no_spaces):
                 if char != chars_txt_no_spaces[i]:
                     num += 1
-            if (len(recog_text_no_spaces)-num-diff)/len(recog_text_no_spaces) > 0.8:
-                print(f"File {file_name} is very likely to be the answer, the difference is {round(1 - (len(recog_text_no_spaces)-num-diff)/len(recog_text_no_spaces), 4)*100}%")
-            diff_dict.update({
-                file_name: int(num+diff)
-            })
+            if (len(recog_text_no_spaces) - num - diff) / len(
+                recog_text_no_spaces
+            ) > 0.8:
+                print(
+                    f"File {file_name} is very likely to be the answer, the difference is {round(1 - (len(recog_text_no_spaces)-num-diff)/len(recog_text_no_spaces), 4)*100}%"
+                )
+            diff_dict.update({file_name: int(num + diff)})
 
         return diff_dict
