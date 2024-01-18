f425d7e2e7663ea31cbd4c06ff0df95a167d834c
gesuyl <60263310+gesuyl@users.noreply.github.com>
2024-01-18 11:30:08 +0100

    change drop_table to empty_table + small fixes

--- app/database.py
+++ app/database.py
@@ -1,4 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, String, Float, Text
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, Float, Text
+from sqlalchemy import MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -46,20 +48,32 @@ class MainConf(Base):
 
 class Database:
     def __init__(self, db_url):
-        self.engine = create_engine(db_url)
+        self.engine = create_engine(db_url)        
+        self.Sessionmaker = sessionmaker(bind=self.engine)
         Base.metadata.create_all(bind=self.engine)
-        self.Session = sessionmaker(bind=self.engine)
+
+        with self.Sessionmaker() as session:
+                    if session.query(MainConf).count() == 0:
+                        default_main_conf = MainConf(img_proc_count=0, precision=0.0)
+                        session.add(default_main_conf)
+                        session.commit()
+
+        print("[#] Database connected")
 
 
     def add_user(self, id, username, role):
-        with self.Session() as session:
-            user = Users(id=id, username=username, role=role)
+        with self.Sessionmaker() as session:
+            user = Users(
+                id=id,
+                username=username,
+                role=role
+            )
             session.add(user)
             session.commit()
 
 
     def delete_user(self, username):
-        with self.Session() as session:
+        with self.Sessionmaker() as session:
             user = session.query(Users).filter_by(username=username).first()
             if not user:
                 return
@@ -68,20 +82,23 @@ class Database:
 
 
     def save_image_data(self, file_name, recog_text):
-        with self.Session() as session:
-            image_data = ImageData(file_name=file_name, recog_text=recog_text)
+        with self.Sessionmaker() as session:
+            image_data = ImageData(
+                file_name=file_name,
+                recog_text=recog_text
+            )
             session.add(image_data)
             session.commit()
 
 
     def save_image_data_batch(self, image_data_list):
-        with self.Session() as session:
+        with self.Sessionmaker() as session:
             session.bulk_save_objects(image_data_list)
             session.commit()
 
 
     def get_images(self, image_id=None):
-        with self.Session() as session:
+        with self.Sessionmaker() as session:
             if image_id:
                 return session.query(ImageData).filter_by(id=image_id).first()
             else:
@@ -89,7 +106,7 @@ class Database:
 
 
     def get_users(self, username=None):
-        with self.Session() as session:
+        with self.Sessionmaker() as session:
             if username:
                 return session.query(Users).filter_by(username=username).first()
             else:
@@ -97,7 +114,7 @@ class Database:
 
 
     def update_attr(self, count=None, precision=None):
-        with self.Session() as session:
+        with self.Sessionmaker() as session:
             conf = session.query(MainConf).first()
             if count:
                 conf.img_proc_count = count
@@ -107,7 +124,7 @@ class Database:
 
 
     def get_image_processing_count(self):
-        with self.Session() as session:
+        with self.Sessionmaker() as session:
             main_conf = session.query(MainConf).first()
             if main_conf:
                 return main_conf.img_proc_count
@@ -115,12 +132,16 @@ class Database:
 
 
     def get_precision(self):
-        with self.Session() as session:
+        with self.Sessionmaker() as session:
             main_conf = session.query(MainConf).first()
             if main_conf:
                 return main_conf.precision
             return 0
 
 
-    def drop_table(self, db_name):
-        Base.metadata.drop_all(bind=self.engine, tables=[db_name])
+    def empty_table(self, table_name):
+        table_to_empty = Base.metadata.tables.get(table_name)
+
+        with self.Sessionmaker() as session:
+            session.execute(table_to_empty.delete())
+            session.commit()
