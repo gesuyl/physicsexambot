5429c708795d0d9976cc0d2af264410477fff97e
gesuyl <60263310+gesuyl@users.noreply.github.com>
2024-01-18 21:30:32 +0100

    small additions to database.py
    - add get_rows_count method to get number of rows in table
    - add table_exists method to check if table exists
    - merge get_image_processing_count and get_precision into one method get_main_conf
    - small fixes

--- app/database.py
+++ app/database.py
@@ -1,6 +1,5 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect, func
 from sqlalchemy import Column, Integer, String, Float, Text
-from sqlalchemy import MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -9,8 +8,8 @@ from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
 
 
-class ImageData(Base):
-    __tablename__ = "image_data"
+class Images(Base):
+    __tablename__ = "images"
     id = Column(Integer, primary_key=True)
     file_name = Column(String, nullable=False)
     recog_text = Column(Text)
@@ -46,13 +45,20 @@ class MainConf(Base):
     precision = Column(Float)
 
 
+    def to_dict(self):
+        return {
+            "img_proc_count": self.img_proc_count,
+            "precision": self.precision,
+            }
+    
+
 class Database:
     def __init__(self, db_url):
-        self.engine = create_engine(db_url)        
-        self.Sessionmaker = sessionmaker(bind=self.engine)
+        self.engine = create_engine(db_url)      
+        self.sessionmaker = sessionmaker(bind=self.engine)
         Base.metadata.create_all(bind=self.engine)
 
-        with self.Sessionmaker() as session:
+        with self.sessionmaker() as session:
                     if session.query(MainConf).count() == 0:
                         default_main_conf = MainConf(img_proc_count=0, precision=0.0)
                         session.add(default_main_conf)
@@ -62,7 +68,7 @@ class Database:
 
 
     def add_user(self, id, username, role):
-        with self.Sessionmaker() as session:
+        with self.sessionmaker() as session:
             user = Users(
                 id=id,
                 username=username,
@@ -73,7 +79,7 @@ class Database:
 
 
     def delete_user(self, username):
-        with self.Sessionmaker() as session:
+        with self.sessionmaker() as session:
             user = session.query(Users).filter_by(username=username).first()
             if not user:
                 return
@@ -82,31 +88,31 @@ class Database:
 
 
     def save_image_data(self, file_name, recog_text):
-        with self.Sessionmaker() as session:
-            image_data = ImageData(
+        with self.sessionmaker() as session:
+            images = Images(
                 file_name=file_name,
                 recog_text=recog_text
             )
-            session.add(image_data)
+            session.add(images)
             session.commit()
 
 
     def save_image_data_batch(self, image_data_list):
-        with self.Sessionmaker() as session:
+        with self.sessionmaker() as session:
             session.bulk_save_objects(image_data_list)
             session.commit()
 
 
     def get_images(self, image_id=None):
-        with self.Sessionmaker() as session:
+        with self.sessionmaker() as session:
             if image_id:
-                return session.query(ImageData).filter_by(id=image_id).first()
+                return session.query(Images).filter_by(id=image_id).first()
             else:
-                return session.query(ImageData).all()
+                return session.query(Images).all()
 
 
     def get_users(self, username=None):
-        with self.Sessionmaker() as session:
+        with self.sessionmaker() as session:
             if username:
                 return session.query(Users).filter_by(username=username).first()
             else:
@@ -114,7 +120,7 @@ class Database:
 
 
     def update_attr(self, count=None, precision=None):
-        with self.Sessionmaker() as session:
+        with self.sessionmaker() as session:
             conf = session.query(MainConf).first()
             if count:
                 conf.img_proc_count = count
@@ -123,25 +129,27 @@ class Database:
             session.commit()
 
 
-    def get_image_processing_count(self):
-        with self.Sessionmaker() as session:
-            main_conf = session.query(MainConf).first()
-            if main_conf:
-                return main_conf.img_proc_count
-            return 0
+    def get_main_conf(self):
+        with self.sessionmaker() as session:
+            return session.query(MainConf).all()
 
-
-    def get_precision(self):
-        with self.Sessionmaker() as session:
-            main_conf = session.query(MainConf).first()
-            if main_conf:
-                return main_conf.precision
-            return 0
 
 
     def empty_table(self, table_name):
         table_to_empty = Base.metadata.tables.get(table_name)
 
-        with self.Sessionmaker() as session:
+        with self.sessionmaker() as session:
             session.execute(table_to_empty.delete())
             session.commit()
+
+
+    def table_exists(self, table_name):
+        insp = inspect(self.engine)
+
+        return insp.has_table(table_name)
+
+
+    def get_row_count(self, table_name):
+        with self.sessionmaker() as session:            
+            return session.query(func.count(f"{table_name}.id")).scalar()
+
